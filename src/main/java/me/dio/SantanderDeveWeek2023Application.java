a7ac86478a43cf648e8f919200863a0f7e2255e0
Matheus Souza <matheussouzati1@gmail.com>
2023-10-22 17:00:41 -0300

    Grandle API REST DIO -INICIO

---
+++ src/main/java/me/dio/SantanderDeveWeek2023Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDeveWeek2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDeveWeek2023Application.class, args);
+	}
+
+}
